29ef0daab812c6b8af09f96b0485d694764e4d8e
Seledkatreid <sexsexseledka@gmail.com>
2025-06-19 17:56:04 +0300

    Add files via upload

--- data_fetch.py
+++ data_fetch.py
@@ -1,17 +1,28 @@
-
 import requests
+import pandas as pd
 from config import API_KEY, SYMBOL, INTERVAL, BASE_URL
 
-def get_price_data():
+def fetch_data():
     params = {
-        'symbol': SYMBOL,
-        'interval': INTERVAL,
-        'apikey': API_KEY,
-        'outputsize': 100
+        "function": "FX_INTRADAY",
+        "from_symbol": SYMBOL.split('/')[0],
+        "to_symbol": SYMBOL.split('/')[1],
+        "interval": INTERVAL,
+        "apikey": API_KEY,
+        "outputsize": "compact"
     }
     response = requests.get(BASE_URL, params=params)
     data = response.json()
-    if 'values' in data:
-        return data['values']
-    else:
-        return []
+
+    if "Time Series FX (" + INTERVAL + ")" not in data:
+        raise Exception("Ошибка получения данных: " + str(data))
+
+    ts_data = data["Time Series FX (" + INTERVAL + ")"]
+    records = []
+    for time, values in ts_data.items():
+        records.append({
+            "datetime": time,
+            "close": float(values["4. close"])
+        })
+    records.sort(key=lambda x: x["datetime"])
+    return records
