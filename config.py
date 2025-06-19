c8042c6dacf0122302cc41cf500745aa644b1a6d
Seledkatreid <sexsexseledka@gmail.com>
2025-06-19 18:02:33 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 TOKEN = "8148012923:AAGMLwawfHBv_fqqMUsf08Odb5kKSiakIKo"
 
 # Настройки для API котировок (например, Alpha Vantage или Libertex)
-API_KEY = "demo"
+API_KEY = YW2H5KYSCUTN8A12
 SYMBOL = "EUR/USD"
 INTERVAL = "1min"
 BASE_URL = "https://www.alphavantage.co/query"
