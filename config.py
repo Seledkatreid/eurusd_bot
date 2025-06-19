9d4e64cfc201d8e25cab58842036505de5e804c8
Seledkatreid <sexsexseledka@gmail.com>
2025-06-19 18:06:23 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Токен Telegram-бота
 TOKEN = "8148012923:AAGMLwawfHBv_fqqMUsf08Odb5kKSiakIKo"
 
-# Настройки для API котировок (например, Alpha Vantage или Libertex)
-API_KEY = YW2H5KYSCUTN8A12
+# Настройки для API котировок (Alpha Vantage)
+API_KEY = "YW2H5KYSCUTN8A12"
 SYMBOL = "EUR/USD"
 INTERVAL = "1min"
 BASE_URL = "https://www.alphavantage.co/query"
